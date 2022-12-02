d6b5c0673c42b802d1e9140ac2d0bae0aded3229
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-12-02 18:14:03 -0300

    feat: add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		box-sizing: border-box;
+	}
+
+	html {
+		font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+		font-size: 62.5%;
+	}
+	
+	html, body, #root {
+		height: 100%;
+	}
+	
+	body {
+		font-size: 1.2rem;
+		background-color: ${({ theme }) => theme && theme.colors['dark-blue']};
+	}
+
+	#root {
+		max-width: 960px;
+		margin: 0 auto;
+	}
+	
+	@media screen and (min-width: 768px) {		
+		body {
+			font-size: 1.6rem;
+		}
+	}
+`
