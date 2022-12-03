684babf3a92a0c9b6b8c0998398f5a07a9588753
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-12-03 02:11:25 -0300

    feat: integrate router, global style and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Header } from './components'
+import { router } from './router'
+import Global from './styles/global'
+import { dark } from './styles/theme/dark'
+
 function App() {
-	return <div className="App"></div>
+	return (
+		<ThemeProvider theme={dark}>
+			<Header />
+			<RouterProvider router={router} />
+			<Global />
+		</ThemeProvider>
+	)
 }
 
 export default App
