a0dad4d45c3d253bf944aa09941a1e067a8989d1
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-12-07 20:09:30 -0300

    feat: remove increase/decrease button from input

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,6 +30,15 @@ export default createGlobalStyle`
 	button {
 		cursor: pointer;
 	}
+
+	input[type=number]::-webkit-inner-spin-button, 
+	input[type=number]::-webkit-outer-spin-button { 
+		-webkit-appearance: none; 
+	}
+
+	input[type=number] {
+		-moz-appearance: textfield;
+	}
 	
 	@media screen and (min-width: 768px) {		
 		body {
