f23787f1fbe7663dba16c6efc50566af60110083
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-01-26 23:17:51 -0300

    feat: add app provider and router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Header } from './components'
-import { router } from './router'
+import { AppProvider } from './context/context'
+import { Router } from './router'
 import Global from './styles/global'
 import { dark } from './styles/theme/dark'
 
 function App() {
 	return (
-		<ThemeProvider theme={dark}>
-			<Header />
-			<RouterProvider router={router} />
-			<Global />
-		</ThemeProvider>
+		<AppProvider>
+			<ThemeProvider theme={dark}>
+				<Router />
+				<Global />
+			</ThemeProvider>
+		</AppProvider>
 	)
 }
 
