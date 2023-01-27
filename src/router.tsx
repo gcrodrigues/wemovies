571da9fbd66c66fe0c4d0f081e845d2b40006846
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-01-26 23:17:37 -0300

    feat: create router component

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,18 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { Home } from './pages'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Header } from './components'
+import { Home, Cart, PurchaseMade } from './pages'
 
-export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Home />,
-	},
-])
+export function Router() {
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="cart" element={<Cart />} />
+					<Route path="success" element={<PurchaseMade />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
+}
